e09e20d03c378642cb742cb7cfd712a34410b9f7
emiffy <ewf1@sfu.ca>
2025-03-13 13:57:55 -0700

    f
    f

---
+++ Assets/Scripts/TeleportFlag.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleportFlag : MonoBehaviour
+{
+    public Transform cameraPos, newPlayerPos;
+   
+}
